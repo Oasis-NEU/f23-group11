4d7abf8f505c3d9e3f8d875432a566428f67ef57
adescoteaux1 <ally.descoteaux.0@gmail.com>
2023-10-15 13:52:41 -0400

    Getting the database

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
+
+import { supabase } from './supabase';
 
 function App() {
+
+  const [ingredients, setIngredients] = useState([]);
+
+  useEffect (() => {
+    getIngredients();
+  }, [])
+
+  async function getIngredients() {
+    try {
+      const { data, error } = await supabase // Destructure the Supabase call
+        .from("Ingredients") // From the "Groceries" table
+        .select("*"); // Select (fetch) everything
+      if (error) throw error; // If there is an error, throw it
+      if (data != null) {
+        // If there is data fetched
+        setIngredients(data); // Set our groceries state variable to the data
+      }
+    } catch (error) {
+      alert(error); // If an error is caught, alert it on the client
+    }
+  }
+
   return (
+    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          App
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
+
+      <div>
+      <h1>Ingredient List</h1>
+      <ul>
+        {ingredients ? (
+          ingredients.map((ingredients) => {
+          return <li key={ingredients.id}>{ingredients.name}</li>
+        })
+      ) : (
+        <p>Loading...</p>
+      )}
+      </ul>
+    </div>
     </div>
   );
 }
