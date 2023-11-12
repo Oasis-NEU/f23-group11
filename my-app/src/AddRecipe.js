51e01c158452189fb387609431b27b19067ea11e
annahiggins1 <122704087+annahiggins1@users.noreply.github.com>
2023-11-12 12:34:01 -0500

    push

---
+++ my-app/src/AddRecipe.js
@@ -0,0 +1,70 @@
+import React from "react";
+
+export default function AddRecipe({
+    recipeName, setRecipeName, ingredients, setIngredients, url, setUrl, picUrl, setPicUrl, category, setCategory
+}) {
+    return (
+        <div className="input">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault(); // stop submit from reloading the page
+              setShowOutput(true);
+            }}
+          >
+            <label htmlFor="form-photo">Photo: </label>
+            <input
+              id="form-photo"
+              type="file"
+              // no `value` because value is a path, but we want to store a photo
+              onChange={(event) => {
+                setPhoto(URL.createObjectURL(event.target.files[0]));
+                setShowOutput(false);
+              }}
+              required
+            />
+            <br />
+    
+            <label htmlFor="form-name">Name: </label>
+            <input
+              id="form-name"
+              type="text"
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value);
+                setShowOutput(false);
+              }}
+              required
+            />
+            <br />
+    
+            <label htmlFor="form-title">Title: </label>
+            <input
+              id="form-title"
+              type="text"
+              value={title}
+              onChange={(e) => {
+                setTitle(e.target.value);
+                setShowOutput(false);
+              }}
+              required
+            />
+            <br />
+    
+            <label htmlFor="form-fun-fact" style={{ verticalAlign: "top" }}>
+              Fun Fact:{" "}
+            </label>
+            {/* <br /> */}
+            <textarea
+              id="form-fun-fact"
+              value={fact}
+              onChange={(e) => {
+                setFact(e.target.value);
+                setShowOutput(false);
+              }}
+            ></textarea>
+            <br />
+            <input type="submit" value="Generate!" />
+          </form>
+        </div>
+      );
+}
