a6f3fc43b0e4f5f643f26022bb50d53435e4e186
annahiggins1 <122704087+annahiggins1@users.noreply.github.com>
2023-11-17 21:16:23 -0500

    added select and deslect based on ingredient type

--- my-app/src/IngredientSection.js
+++ my-app/src/IngredientSection.js
@@ -6,8 +6,37 @@ export default function IngredientSection({
     category,
     showRecipes
 }) {
+    async function selectAllIngredients() {
+        var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+        checkboxes.forEach((checkbox) => {
+          const ingredientType = checkbox.getAttribute('data-cat');
+          console.log("TYPE", checkbox.cat);
+          if (ingredientType === category) {
+            checkbox.checked = true;
+          }
+        });
+        showUserIngredients();
+        showRecipes();
+      }
+      
+      async function deselectAllIngredients() {
+        var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+        checkboxes.forEach((checkbox) => {
+          const ingredientType = checkbox.getAttribute('data-cat');
+          if (ingredientType === category) {
+            checkbox.checked = false;
+          }
+        });
+        showUserIngredients();
+        showRecipes();
+      }
+
     const formattedCategory = category.charAt(0).toUpperCase() + category.slice(1);
     return (<div className="column" style={{ flex: '1' }}>
+    <div>
+        <button onClick={() => selectAllIngredients()}>Select All {formattedCategory}</button>
+        <button onClick={() => deselectAllIngredients()}>Deselect All {formattedCategory}</button>
+    </div>
     <h1>{formattedCategory}</h1>
     {ingredients &&
       ingredients.map((ingredient) => (
@@ -19,6 +48,7 @@ export default function IngredientSection({
                 id={ingredient.id}
                 name={ingredient.name}
                 value={ingredient.name}
+                data-cat={ingredient.ingredientType}
                 onChange={(e) => (showUserIngredients(e) && showRecipes(e))}
               />
               <label htmlFor={ingredient.id}>{ingredient.name}</label>
